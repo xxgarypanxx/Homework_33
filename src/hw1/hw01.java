34bc09b3e336f2b06830578c110e6c3731e6cdc0
Gary Pan <garypan0327@gmail.com>
2023-09-04 16:32:18 +0800

    push hw1 to github

---
+++ src/hw1/hw01.java
@@ -0,0 +1,43 @@
+package hw1;
+
+public class hw01 {
+	public static void main (String[]args){
+	/*	int d = 12; int t = 6;
+	System.out.println(t+d);
+	System.out.println(t*d);
+	*/
+	//===============
+	
+	/*int egg =200; int box = 12;
+	System.out.println(egg/box);
+	System.out.println(egg%box);
+	
+	*/
+	/*	int time = 256559; int day= 60*60*24;
+		int min = 60; int hour =60*60;
+		
+				
+		System.out.println(time/day);
+		System.out.println(time%day/hour);
+		System.out.println(time%day%hour/min);
+		System.out.println(time%day%hour%min); */
+
+
+	int math = 5; double pi =3.1415;
+	System.out.println(math*math*pi);
+	System.out.println(math*2*pi);
+
+	
+	
+	
+	}
+
+
+
+
+
+
+}
+
+
+
